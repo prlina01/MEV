a668259c24f2d54a975ea37370cd0aaa719238b2
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-06-08 12:26:28 +0200

    feat: Connecting to the PostgreSQL database.

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -4,11 +4,18 @@ import (
 	"authentication/data"
 	"database/sql"
 	"fmt"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "80"
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -18,7 +25,15 @@ type Config struct {
 func main() {
 	log.Println("Starting authentication service")
 
-	app := Config{}
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres!")
+	}
+
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(webPort),
@@ -28,5 +43,43 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready ...")
+			counts++
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds....")
+		time.Sleep(2 * time.Second)
+		continue
 	}
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -7,3 +7,16 @@ require (
 	github.com/go-chi/cors v1.2.1
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 )
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
